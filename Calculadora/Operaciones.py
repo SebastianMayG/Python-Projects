8b98f8c13270683466cd7b0b67f9c0822113a7ed
SebastianMayG <al064488@uacam.mx>
2023-12-10 14:17:57 -0600

    se crearon clases

--- Calculadora/Operaciones.py
+++ Calculadora/Operaciones.py
@@ -1,14 +1,16 @@
-def Suma(a , b):
-    return a + b
+class Operaciones:
 
-def Resta(a , b):
-    return a - b
+    def Suma(a , b):
+        return a + b
 
-def Multiplicacion(a , b):
-    return a * b
+    def Resta(a , b):
+        return a - b
 
-def Division(a , b):
-    return a / b
+    def Multiplicacion(a , b):
+        return a * b
 
-def Potencia(a , b):
-    return a ** b
+    def Division(a , b):
+        return a / b
+
+    def Potencia(a , b):
+        return a ** b
